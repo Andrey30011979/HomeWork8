b3c4ee863516269c264b528af675f13743b529a7
Andrey Marymov <marymovandrey078115@gmail.com>
2022-09-15 20:32:01 +0300

    Task 60

--- Task60/Program.cs
+++ Task60/Program.cs
@@ -7,7 +7,7 @@
 // 27(0,0,1) 90(0,1,1)
 // 26(1,0,1) 55(1,1,1)
 
-void Print3D(int[,,] array)
+/*void Print3D(int[,,] array)
 {
     for (int x = 0; x < array.GetLength(0); x++)
     {
@@ -24,7 +24,7 @@ void Print3D(int[,,] array)
     Console.WriteLine();
 }
 
-int[,,] mass = new int[3, 4, 5];
+int[,,] mass = new int[2, 3, 4];
 int value = 10;
 
 if (mass.Length>(99-10))
@@ -46,4 +46,74 @@ else
     }
     Print3D(mass);
 }
+*/
+int arraySizeX = 2;
+int arraySizeY = 2;
+int arraySizeZ = 2;
+int minNumber = 10;
+int maxNumber = 99;
+int[,,] testArray = new int[arraySizeX, arraySizeY, arraySizeZ];
 
+Console.Clear();
+FillArrayRandomNumber(testArray, minNumber, maxNumber);
+PrintArrayWithIdex(testArray);
+
+void FillArrayRandomNumber(int[,,] array, int minNumber = 0, int maxNumber = 9)
+{
+    Random rand = new Random();
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int h = 0; h < array.GetLength(2); h++)
+            {
+                while (array[i, j, h] == 0)
+                {
+                    int number = rand.Next(minNumber, maxNumber + 1);
+
+                    if (IsNumberInArray(array, number) == false)
+                    {
+                        array[i, j, h] = number;
+                    }
+                }
+
+            }
+        }
+    }
+}
+
+bool IsNumberInArray(int[,,] array, int number)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int h = 0; h < array.GetLength(2); h++)
+            {
+                if (array[i, j, h] == number) return true;
+            }
+        }
+    }
+
+    return false;
+}
+
+void PrintArrayWithIdex(int[,,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int h = 0; h < array.GetLength(2); h++)
+            {
+                Console.Write(array[i, j, h]);
+                Console.Write("({0},{1},{2})\t", i, j, h);
+            }
+
+            Console.WriteLine();
+        }
+
+        Console.WriteLine();
+    }
+}
